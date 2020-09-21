824beda5d7c1e5004adefcb9d8006c45b644df01
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-21 12:49:22 +0200

    Add project files.

---
+++ Pra.OefeningKlassenFields.CORE/Kaart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pra.OefeningKlassenFields.CORE
+{
+    public enum KaartKleur { rood, zwart }
+    public enum KaartSoort { ruiten, harten, schoppen, klavers }
+    public class Kaart
+    {
+        public KaartKleur kleur;  // hier komt rood of zwart
+        public KaartSoort soort;  // hier komt ruiten, harten, schoppen of klavers
+        public int waarde; // hier komt de waarde 1, 2, 3 ... 11 (voor boer), 12 (voor dame), 13 (voor heer)
+        public int bonus;  // harten => bonus = 0, ruiten => bonus = 2, klavers => bonus = 4, schoppen => bonus = 6
+
+        public Kaart(KaartSoort soort, int waarde)
+        {
+            if (soort == KaartSoort.harten)
+            {
+                kleur = KaartKleur.rood;
+                bonus = 0;
+            }
+            else if (soort == KaartSoort.ruiten)
+            {
+                kleur = KaartKleur.rood;
+                bonus = 2;
+            }
+            else if (soort == KaartSoort.klavers)
+            {
+                kleur = KaartKleur.zwart;
+                bonus = 4;
+            }
+            else
+            {
+                kleur = KaartKleur.zwart;
+                bonus = 6;
+            }
+            this.soort = soort;
+            this.waarde = waarde;
+        }
+    }
+
+}
---
+++ Pra.OefeningKlassenFields.WPF/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pra.OefeningKlassenFields.CORE;
+
+namespace Pra.OefeningKlassenFields.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rnd = new Random();
+        }
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Kaart mijnKaart = MaakKaart();
+            Kaart computerKaart = MaakKaart();
+
+            lblMijnKaartKleur.Content = mijnKaart.kleur.ToString();
+            lblMijnKaartSoort.Content = mijnKaart.soort.ToString();
+            lblMijnKaartBonus.Content = mijnKaart.bonus.ToString();
+            lblMijnKaartWaarde.Content = mijnKaart.waarde.ToString();
+            int mijnScore = mijnKaart.waarde + mijnKaart.bonus;
+            lblMijnKaartScore.Content = mijnScore.ToString();
+
+            lblComputerKaartKleur.Content = computerKaart.kleur.ToString();
+            lblComputerKaartSoort.Content = computerKaart.soort.ToString();
+            lblComputerKaartBonus.Content = computerKaart.bonus.ToString();
+            lblComputerKaartWaarde.Content = computerKaart.waarde.ToString();
+            int computerScore = computerKaart.waarde + computerKaart.bonus;
+            lblComputerKaartScore.Content = computerScore.ToString();
+
+            if (mijnScore > computerScore)
+                lblResultaat.Content = "Je wint";
+            else if (mijnScore < computerScore)
+                lblResultaat.Content = "Je verliest";
+            else
+                lblResultaat.Content = "Onbeslist";
+        }
+
+        private Kaart MaakKaart()
+        {
+            int soort = rnd.Next(1, 5);
+            // 1 = schoppen
+            // 2 = klavers
+            // 3 = harten
+            // 4 = ruiten
+            KaartSoort kaartsoort = KaartSoort.ruiten;
+            if (soort == 1) kaartsoort = KaartSoort.schoppen;
+            if (soort == 2) kaartsoort = KaartSoort.klavers;
+            if (soort == 3) kaartsoort = KaartSoort.harten;
+            int waarde = rnd.Next(1, 14);
+            return new Kaart(kaartsoort, waarde);
+
+        }
+    }
+}
